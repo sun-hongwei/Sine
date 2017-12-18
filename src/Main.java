323931a58ffd8c06280aff5f56f1482a5ba09111
sunhongwei <1012191814@qq.com>
2017-12-18 09:41:03 +0800

    实现正弦和余弦长短接头

--- src/Main.java
+++ src/Main.java
@@ -17,31 +17,37 @@ public class Main extends JPanel {
         // TODO Auto-generated method stub
         super.paintComponent(g);
         g.setColor(Color.WHITE);//设置面板背景色
-        g.fillRect(500, 800, 500, 500);//填充面板
+        g.fillRect(0, 0, 1200, 300);//填充面板
         g.setColor(Color.RED);//设置画线的颜色
-        for (sinX = 0; sinX <= 2000; sinX += 0.1)//一个周期
+        for (sinX = 0; sinX <= 1000; sinX += 0.1)//一个周期
         {
-            //sin
-            sinY = Math.sin(sinX * Math.PI / 180);//转化为弧度,1度=π/180弧度
-            sinY = (80 + 80 * sinY);//便于在屏幕上显示
-            g.drawLine((int) sinX, (int) sinY+50, (int) sinX, (int) sinY+50);//画
+            if(sinX > 300){
+                //sin
+                sinY = Math.sin(sinX * Math.PI / 180);//转化为弧度,1度=π/180弧度
+                sinY = (80 + 80 * sinY);//便于在屏幕上显示
+                g.drawLine((int) sinX, (int) sinY+50, (int) sinX, (int) sinY+50);//画
+            }
+
         }
         g.setColor(Color.BLACK);//设置画线的颜色
-        for (cosX = 0; cosX <= 2000; cosX += 0.1)//一个周期
+        for (cosX = 0; cosX <= 1000; cosX += 0.1)//一个周期
         {
-            //cos
-            cosY = Math.cos(cosX * Math.PI / 180);//转化为弧度,1度=π/180弧度
-            cosY = (80 + 80 * cosY);//便于在屏幕上显示
-            g.drawLine((int) cosX + 250, (int) cosY+50, (int) cosX + 250, (int) cosY+50);//画点
+            if(cosX < 700){
+                //cos
+                cosY = Math.cos(cosX * Math.PI / 180);//转化为弧度,1度=π/180弧度
+                cosY = (80 + 80 * cosY);//便于在屏幕上显示
+                g.drawLine((int) cosX + 250, (int) cosY+50, (int) cosX + 250, (int) cosY+50);//画点
+            }
         }
     }
 
     public static void main(String[] args) {
         Main s = new Main();
         JFrame j = new JFrame();
-        j.setTitle("一个周期的正弦曲线");
+        j.setTitle("正弦加余弦");
         j.add(s);
-        j.setSize(500, 500);
+        j.setSize(1300, 500);
+        j.setLocation(300,300);
         j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         j.setVisible(true);
     }
